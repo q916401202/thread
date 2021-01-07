b9ed1f3fddcd854854ddcf240b14ae53b5e8f8f4
liushaoqing <q916401202@163.com>
2021-01-07 14:32:39 +0800

    fix

---
+++ thread01/src/main/java/com/qing/thread01/Thread01Application.java
@@ -0,0 +1,16 @@
+package com.qing.thread01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author qing
+ */
+@SpringBootApplication
+public class Thread01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Thread01Application.class, args);
+	}
+
+}

425500e40aabfa2048cec7a27d09b3a4381b88f8
liushaoqing <q916401202@163.com>
2021-01-12 16:37:48 +0800

    fix

---
+++ thread02/src/main/java/com/qing/thread02/Thread02Application.java
@@ -0,0 +1,13 @@
+package com.qing.thread02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Thread02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Thread02Application.class, args);
+	}
+
+}
---
+++ thread02/src/test/java/com/qing/thread02/Thread02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qing.thread02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Thread02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
